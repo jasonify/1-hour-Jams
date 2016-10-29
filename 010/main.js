8e46662fc37e78886525bd32a3dae0a3ea2b36b1
jason <bjason@gmail.com>
2016-10-29 13:11:48 -0700

    aniamte

--- 010/main.js
+++ 010/main.js
@@ -2,8 +2,17 @@ var circle = d3.selectAll('circle');
 circle.style('fill', 'purple');
 
 
+function animate(){
+
 
 circle.attr("cy", function(){
-  return Math.random() * 700;
+  return Math.random() * 500;
 });
 
+  setTimeout(function(){
+    requestAnimationFrame(animate);
+  }, 1000/20);
+}
+
+
+animate();
